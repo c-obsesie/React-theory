93ac77e82b17af72550caf288990b3aec9c76f0e
c-obsesie <56051151+c-obsesie@users.noreply.github.com>
2020-03-17 15:05:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+/* React :
+-virtual DOM 
+-reusable web components 
 
+Syntax:
+ReactDOM.render(What do I want to render(e.g. component), Where do I want to render(e.g. root))
+
+import React form 'react'
+import ReactDOM from 'react-dom'
+
+From JSX to JS
+<h1> Hello {JS}</h1>
+or
+<h1> Hello {`${JS}`}</h1>
+
+Insert style in React
+
+v1. <h1 style={{color:"#ffff"}}>Hello World</h1>
+v2. const styles ={background:black}
+<h1 style={stlye}>Hello </h1>
