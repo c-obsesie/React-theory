f6e2d303efcefad9d310a4138b6c562b8deb22c5
Cosmin Mihalache <56051151+c-obsesie@users.noreply.github.com>
2020-03-19 18:22:57 +0000

    Update JSX.js

--- JSX.js
+++ JSX.js
@@ -1,10 +1,7 @@
-
-
 //Variable Attributes in JSX
 
 const goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg';
 
-// Declare new variable here:
 var gooseImg=<img src={goose} />
 ReactDOM.render(gooseImg,document.getElementById('app').)
 
