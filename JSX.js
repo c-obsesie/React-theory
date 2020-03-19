0e73ef2e3a6b1026060f61a75665c46d27ff156d
Cosmin Mihalache <56051151+c-obsesie@users.noreply.github.com>
2020-03-19 18:32:25 +0000

    Update JSX.js

--- JSX.js
+++ JSX.js
@@ -5,3 +5,23 @@ const goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_p
 var gooseImg=<img src={goose} />
 ReactDOM.render(gooseImg,document.getElementById('app').)
 
+// If Else in JSX
+
+if (user.age >= drinkingAge) {
+  message = (
+    <h1>
+      Hey, check out this alcoholic beverage!
+    </h1>
+  );
+} else {
+  message = (
+    <h1>
+      Hey, check out these earrings I got at Claire's!
+    </h1>
+  );
+}
+
+ReactDOM.render(
+  message, 
+  document.getElementById('app')
+);
